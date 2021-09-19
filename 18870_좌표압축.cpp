4121612e50e8a61f552f37255140c980730a9fa4
SuJin <1127sujink@naver.com>
2021-09-19 21:50:48 +0900

    Update 18870_좌표압축.cpp

--- 18870_좌표압축.cpp
+++ 18870_좌표압축.cpp
@@ -6,10 +6,15 @@ int main()
 {
     int n;
     int x[10000000];
+    int copy[10000000];
 
     scanf("%d", &n);
     for(int i = 0; i < n; i++)
-        scanf("%d", &x[i]);
+    {
+        scanf("%d", &x[i]); 
+        copy[i] = x[i];
+    }
     
+    sort(copy, copy + n);
     
 }
